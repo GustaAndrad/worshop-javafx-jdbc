b7debff679fb9fc50447d181e27a4c4fd4d4ac36
Gustavo Andrade <gusta.andradev@gmail.com>
2022-01-14 14:21:36 -0300

    Main view controller

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
