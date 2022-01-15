44fed9c5b283e9f4dacaf5aea946472e360dc09e
Gustavo Andrade <gusta.andradev@gmail.com>
2022-01-15 14:05:12 -0300

    Observer design pattern to update tableview

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,15 +1,22 @@
 package gui;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import db.DbException;
+import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import model.entities.Department;
 import model.services.DepartmentService;
 
@@ -19,6 +26,8 @@ public class DepartmentFormController implements Initializable {
 
 	private DepartmentService service;
 	
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
+		
 	@FXML
 	private TextField txtId;
 	
@@ -42,12 +51,35 @@ public class DepartmentFormController implements Initializable {
 		this.service = service;
 	}
 	
-	@FXML
-	public void onBtSaveAction() {
-		entity = getFormData();
-		service.saveOrUpdate(entity);
+	public void subscribeDataChangeListenner(DataChangeListener listener) {
+		dataChangeListeners.add(listener);
 	}
 	
+	@FXML
+	public void onBtSaveAction(ActionEvent event) {
+		if(entity == null) {
+			throw new IllegalStateException("Entity was null.");
+		}
+		if(service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		try {
+		entity = getFormData();
+		service.saveOrUpdate(entity);
+		notifyDataChangeListeners();
+		Utils.currentStage(event).close();
+		}
+		catch (DbException e) {
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		}
+	}
+	
+	private void notifyDataChangeListeners() {
+		for (DataChangeListener listener : dataChangeListeners) {
+			listener.onDataChange();
+		}		
+	}
+
 	private Department getFormData() {
 		Department obj = new Department();
 		
@@ -58,8 +90,8 @@ public class DepartmentFormController implements Initializable {
 	}
 
 	@FXML
-	public void onBtCancelAction() {
-		
+	public void onBtCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
 	}
 	
 	@Override
